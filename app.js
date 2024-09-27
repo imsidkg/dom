f110421b8089815a018587b5deed1546e74846b6
imsidkg <siddharth8271skg2@gmail.com>
2024-09-27 15:57:42 +0530

    private repo

---
+++ app.js
@@ -0,0 +1,17 @@
+//selecting element
+
+//getElementById
+
+// const title = document.getElementById("main-heading");
+// console.log(title);
+
+//getElementByClassname
+
+// const listItems = document.getElementsByClassName('list-items')
+// console.log(listItems)
+// const listItems = document.getElementsByTagName('li')
+// console.log(listItems)
+
+
+const item = document.querySelectorAll('div')
+console.log(item)
