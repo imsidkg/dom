810eceed1606ab6f1f544851e90459eef3833204
Siddharth Kumar Gope <siddharth8271skg2@gmail.com>
2025-06-28 23:04:10 +0530

    checking new fiels

--- dom/app.js
+++ dom/app.js
@@ -1,15 +1,1 @@
-const ul = document.querySelector('ul');
-const li = document.querySelector('li');
-
-ul.append(li);
-
-li.innerText= 'X-Men';
-
-li.setAttribute('class' , 'list-items')
-
-console.log('added for test');
-API_KEY
-
-
-console.log(hola)
-API_KEY_DEEPSEEK = 'invalid'
+dslfasdfljasdlfjsdk 
